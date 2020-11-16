2c903a26ed95471f9b8d3c555bff5146ae6ba81b
LawrenceScroggs <lawrencescroggs@gmail.com>
2020-11-15 20:41:35 -0800

    worked on building the .h files I was assigned.  It has been awhile since I have messed around with OOP

--- entity.h
+++ entity.h
@@ -1,4 +1,48 @@
-// This .h file will hold the entity class 
 // Lawrence Scroggs -- 11/13/2020 -- CS300:Frupal:Group 4
+// This .h file will hold the entity class 
 
 
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+
+
+using namespace std;
+
+
+class entity{
+
+  string name;
+  int * location;
+  char type;
+
+};
+class player:public entity{
+
+  int my_whiffles;
+  int my_energy;
+  item * my_items;
+
+}
+class object:public entity{
+
+  int energy_needed;
+
+};
+class royal_diamonds:public entity{
+
+  string something;
+  bool game_end;
+
+};
+class item:public entity{
+
+  int whiffles;
+  int owned;
+
+}
+class clue:public entity{
+
+  String sentence;
+
+};
