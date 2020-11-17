1cccc445d38a90a6e505ab39279278dfe488220d
Taylor Noah <tnoah@ada.cs.pdx.edu>
2020-11-16 17:03:39 -0800

    created map.h. #includes player.h. contains class Grovnik and class Map. Some ideas for function prototypes also included.

---
+++ Map.h
@@ -0,0 +1,41 @@
+//Taylor Noah
+//CS300, Group Project: Land of Frupal
+//Group 4 et al
+
+#include "Player.h"
+
+class Grovnik
+{
+    private:
+        
+        bool isVisable;
+        //could be obj, item, etc.
+        Entity* entity;
+
+    public:
+        
+        enum type {meadow = 0, swamp = 1, water = 2, wall = 3, diamond = 4};
+
+        //Grovnik();
+        //~Grovnik();
+        
+
+
+};
+
+
+class Map
+{
+    private:
+
+        Grovnik** map;
+        //player???
+        Entity* ent;
+
+    public:
+
+        //Map();
+        //~Map();
+
+        //initMap1();
+};
