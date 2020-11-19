2f7b6b58fdebfbae6624ad0a82b67d609e23cca6
Taylor Noah <tnoah@ada.cs.pdx.edu>
2020-11-18 19:46:16 -0800

    create menu.h. add int** map and String filename

---
+++ menu.h
@@ -0,0 +1,22 @@
+//CS300, Group Project: Land of Frupal
+//Group 4 et al
+
+//This is the Menu header file
+//It contains the menu class
+
+
+#include "map.h"
+
+class Menu
+{
+    private:
+
+        //2D pointers for flexible size
+        int** map;
+        String filename;
+
+    public:
+
+        map();
+        ~map();
+}
