f05d60273ea9e5b3620fece0d430c39100dcc22d
Taylor Noah <tnoah@ada.cs.pdx.edu>
2020-12-03 14:50:29 -0800

    add binolcuar and ship to item.init

--- item.cpp
+++ item.cpp
@@ -44,7 +44,7 @@ void Tools::init(int i)
     if( i == 0)
     {
         rating = 2;
-        whiffles = 25;
+        whiffles = 50;
         name = "Axe";
     }
 
@@ -52,8 +52,21 @@ void Tools::init(int i)
     else if( i == 1)
     {
         rating = 3;
-        whiffles = 40;
+        whiffles = 100;
         name = "Hammer";
+    }
+
+    //create binocluars
+    else if( i == 2)
+    {
+        name = "Binoculars";
+        whiffles = 500;
+    }
+
+    else if ( i == 3)
+    {
+        name = "Ship";
+        whiffles = 1000;
     }
 }
 
